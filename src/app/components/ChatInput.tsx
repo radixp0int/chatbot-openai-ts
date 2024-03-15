bf069598f1a38df65d522df76f09c977d2d2d84e
Christian Sifuentes <me@radixpoint.dev>
2024-03-15 07:29:10 -0400

    refactoring and adding button

--- src/app/components/ChatInput.tsx
+++ src/app/components/ChatInput.tsx
@@ -1,15 +1,18 @@
 "use client";
+
 import { MessagesContext } from "@/context/messages";
 import { cn } from "@/lib/utils";
 import { Message } from "@/lib/validators/message";
 import { useMutation } from "@tanstack/react-query";
+import { CornerDownLeft, Loader2 } from "lucide-react";
 import { nanoid } from "nanoid";
-import React, { HTMLAttributes, useContext, useRef, useState } from "react";
+import { FC, HTMLAttributes, useContext, useRef, useState } from "react";
+// import { toast } from "react-hot-toast";
 import TextareaAutosize from "react-textarea-autosize";
 
 interface ChatInputProps extends HTMLAttributes<HTMLDivElement> {}
 
-const ChatInput = ({ className, ...props }: ChatInputProps) => {
+const ChatInput: FC<ChatInputProps> = ({ className, ...props }) => {
   const [input, setInput] = useState<string>("");
   const {
     messages,
@@ -21,7 +24,8 @@ const ChatInput = ({ className, ...props }: ChatInputProps) => {
 
   const textareaRef = useRef<HTMLTextAreaElement>(null);
 
-  const { mutate: sendMessage } = useMutation({
+  const { mutate: sendMessage, isLoading } = useMutation({
+    mutationKey: ["sendMessage"],
     mutationFn: async (message: Message) => {
       const response = await fetch("/api/message", {
         method: "POST",
@@ -87,11 +91,25 @@ const ChatInput = ({ className, ...props }: ChatInputProps) => {
             }
           }}
           maxRows={4}
+          disabled={isLoading}
           value={input}
           onChange={(e) => setInput(e.target.value)}
           autoFocus
           placeholder="Write a message..."
           className="peer disabled:opacity-50 pr-14 resize-none block w-full border-0 bg-zinc-100 py-1.5 text-gray-900 focus:ring-0 text-sm sm:leading-6"
+        />
+        <div className="absolute inset-y-0 right-0 flex py-1.5 pr-1.5">
+          <kbd className="inline-flex items-center rounded border bg-white border-gray-200 px-1 font-sans text-xs text-gray-400">
+            {isLoading ? (
+              <Loader2 className="w-3 h-3 animate-spin" />
+            ) : (
+              <CornerDownLeft className="w-3 h-3" />
+            )}
+          </kbd>
+        </div>
+        <div
+          aria-hidden="true"
+          className="absolute inset-x-0 bottom-0 border-t border-gray-300 peer-focus:border-t-2 peer-focus:border-indigo-600"
         />
       </div>
     </div>
