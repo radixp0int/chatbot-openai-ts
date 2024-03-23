c329c5a5d086113df4b8d5815cf9301714dde4d4
Christian Sifuentes <me@radixpoint.dev>
2024-03-22 21:19:08 -0400

    fixing tailwind var missing

--- src/app/components/ChatAssistant.tsx
+++ src/app/components/ChatAssistant.tsx
@@ -8,24 +8,28 @@ type Props = {}
 
 const ChatAssistant = (props: Props) => {
   return (
-    <Accordion type='single' collapsible className='relative bg-white z-40 shadow'>
-      <AccordionItem value='item-1'>
-        <div className='fixed right-8 w-80 bottom-8 bg-white border border-gray-200 rounded-md overflow-hidden'>
-          <div className='w-full h-full flex flex-col'>
-            <AccordionTrigger className='px-6 border-b border-zinc-300'>
+    <Accordion
+      type="single"
+      collapsible
+      className="relative bg-white z-40 shadow"
+    >
+      <AccordionItem value="item-1">
+        <div className="fixed right-8 w-80 bottom-8 bg-white border border-gray-200 rounded-md overflow-hidden">
+          <div className="w-full h-full flex flex-col">
+            <AccordionTrigger className="px-6 border-b border-zinc-300">
               <ChatHeader />
             </AccordionTrigger>
             <AccordionContent>
-              <div className='flex flex-col h-80'>
-                <ChatMessage className='px-2 py-3 flex-1' />
-                <ChatInput className='px-4' />
+              <div className="flex flex-col h-96">
+                <ChatMessage className="px-2 py-3 flex-1" />
+                <ChatInput className="px-4" />
               </div>
             </AccordionContent>
           </div>
         </div>
       </AccordionItem>
     </Accordion>
-  )
+  );
 }
 
 export default ChatAssistant
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -36,27 +36,6 @@ const config = {
           DEFAULT: "hsl(var(--destructive))",
           foreground: "hsl(var(--destructive-foreground))",
         },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
         "accordion-down": {
@@ -71,6 +50,11 @@ const config = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
