1af7be471c6918f6d1d61dfafa88541cb9129bb4
Christian Sifuentes <me@radixpoint.dev>
2024-07-20 06:41:37 -0400

    update icon to be used as button

--- src/app/components/ChatAssistant.tsx
+++ src/app/components/ChatAssistant.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import {
   Accordion,
   AccordionContent,
@@ -8,10 +9,17 @@ import {
 import ChatHeader from "./ChatHeader";
 import ChatInput from "./ChatInput";
 import ChatMessage from "./ChatMessages";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 type Props = {};
 
 const ChatAssistant = (props: Props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <Accordion
       type="single"
@@ -19,12 +27,34 @@ const ChatAssistant = (props: Props) => {
       className="relative bg-white z-40 shadow"
     >
       <AccordionItem value="item-1">
-        <div className="fixed right-8 w-96 bottom-8 bg-white border border-gray-200 rounded-md overflow-hidden">
+        <div
+          className={`fixed right-8 bottom-8 ${
+            isOpen
+              ? "w-96 bg-white border border-gray-200 rounded-md overflow-hidden"
+              : ""
+          }`}
+        >
           <div className="w-full h-full flex flex-col">
-            <AccordionTrigger className="px-6 border-b border-zinc-300">
-              <ChatHeader />
+            <AccordionTrigger
+              className={`${isOpen ? "px-6 border-b border-zinc-300" : ""}`}
+              onClick={handleToggle}
+            >
+              {isOpen ? (
+                <ChatHeader />
+              ) : (
+                <Avatar
+                  className={`transition-opacity duration-300 ease-in-out ${
+                    isOpen ? "opacity-0 invisible" : "opacity-100 visible w-16 h-16"
+                  } fixed right-8 bottom-8`}
+                >
+                  <AvatarImage src="/chatbot.png" alt="chatbot"/>
+                  <AvatarFallback delayMs={600}>AI</AvatarFallback>
+                </Avatar>
+              )}
             </AccordionTrigger>
-            <AccordionContent>
+            <AccordionContent
+              className={`transition-max-height transition-opacity duration-300 ease-in-out ${isOpen ? "open opacity-100 visible" : "opacity-0 invisible"}`}
+            >
               <div className="flex flex-col h-128">
                 <ChatMessage className="px-2 py-3 flex-1" />
                 <ChatInput className="px-4" />
